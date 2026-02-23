de3183da430163d66a7d30ddc91fc0b0c3b2440a
KSamuel2001 <ksamuel5@emich.edu>
2026-02-23 14:39:09 -0500

    programmer 2 added a new print

--- src/eclipseSoloProj/HelloWorld.java
+++ src/eclipseSoloProj/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
  public static void main(String[] args) {
 	 System.out.println("Hello World");
 	 System.out.println("Im awwesome");
+	 System.out.println("Programmer 2 did this");
  }
 }
